d8be97bd41084e0ed5bacbb58ebde79e9b370b07
Geraldo Costa <geraldocoosta@gmail.com>
2019-10-11 00:18:12 -0300

    projeto consumindo cookie

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <form action>
+      <input type="text" v-model="usuario.nome"/>
+      <input type="password" v-model="usuario.pass"/>
+      <button type="submit" @click.prevent="submit"> submit </button>
+      <button type="submit" @click.prevent="submitAgain"> teste </button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data(){
+    return {
+      usuario: {}
+    }
+  },
+  methods: {
+    submit(){
+      axios.post('http://localhost:8080/mercado/login', this.usuario, {withCredentials: true})
+        .then(res => console.log('sucesso', res.data))
+        .catch(e => console.error(e));
+    },
+    submitAgain(){
+      axios.get('http://localhost:8080/mercado/login/teste',{withCredentials: true})
+        .then(res => console.log('sucesso', res.data))
+        .catch(e => console.error(e));
+    },
+  }
+};
+</script>
